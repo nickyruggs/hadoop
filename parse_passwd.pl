616a7a753cfb2de895062a859f3c7713fc48ef1d
[Nicholas Ruggiero] <[Nicholas.Ruggiero@dell.com]>
2020-10-30 09:38:28 -0400

    Updated parse passwd to set --enabled flag for users

--- parse_passwd.pl
+++ parse_passwd.pl
@@ -6,32 +6,31 @@
 #  This comes into play when trying to map isilon to an existing installation.
 #
 #  IMPORTANT: Edit the passwd and group files and delete the unwanted users...
-#             The hadoop service account users are usually grouped together.
+#             These users are usually grouped together.
 #
 #  Questions and inquiries:  Nicholas Ruggiero
 #                            nicholas.ruggiero@emc.com
 #  
 #  Warnings:  This is not bullet proof, please review the resulting isi commands
-#             file to ensure desired result.  best practice is to cut and paste
-#             the commands on the isilon command line.
+#             file to ensure desired result.
 #
 #  Version History:
-#             v. 0.91      3/28/15         Initial Version
-#             v. 0.92      8/4/15          input defaults updated
-#             v. 0.93      4/21/16         removed dist parameter, and force add of supergroup
-#
+#             v. 0.1      3/28/15         Initial Version
+#             v. 1.1      7/27/2020       Added --enabled = true as required since 8.2
+#                                         fixed issue with modify groups coming before 
+#                                         the user was created.
 #
 use strict;
 use Getopt::Long;
 #
 #   input section
 #
-my $passwd="";              # local file name
-my $group="";                # local file name of /etc/group
+my $passwd="passwd";              # local file name
+my $group="group";                # local file name of /etc/group
 my $isi_cmds="isi_commands.txt";  # Output file for the isi commands
-my $zone="system";                      # Access zone on the isilon system for this hadoop
-my $hdfs_root="/ifs/";            # HDFS Root for the above access zone
-#my $dist="";                   # Hadoop Distribution  - cdh|hwx|phd|ibm
+my $zone="";                      # Access zone on the isilon system for this hadoop cluster
+my $hdfs_root="/ifs/";            # HDFS Root for the above access zone 
+my $dist="cdh";                   # Hadoop Distribution  - cdh|hwx|phd
 #
 #
 #  Variable declaration
@@ -45,48 +44,55 @@ my $got_super="TRUE";
 my $gid="";
 my $users="";
 my @commands;
+my @mod_commands;
 my @file;
+my $ier;
 #
 #  for getopt
 #
 my $help=0;
-my $version="0.93";
+my $version="1.2";
 #
-GetOptions ('passwd=s' => \$passwd, 
+$ier=GetOptions ('passwd=s' => \$passwd, 
 	    'group=s' => \$group, 
 	    'hdfs_root=s' => \$hdfs_root, 
 	    'zone=s' => \$zone, 
+	    'dist=s' =>\$dist,
 	    'help' => \$help);
-if ($help) {
-    print "\nTo the rescue...BTW, this is version $version\n\n";
-    print "Usage: parse_passwd.pl --passwd <filename> --group <filename> --hdfs_root <hdfs root directory> --zone <access zone> \n\n";
+#
+if ($help || $ier != 1 ) {
+    print "Usage: parse_passwd.pl --passwd <filename> --group <filename> --hdfs_root <hdfs root directory> --zone <access zone>  --dist cdh|hwx|phd\n\n";
     exit;
 }
 #
 #  Okie dokie here we go....
 #
-print "...and begin\n";
-print "passwd file \t$passwd\n";
-print "group file  \t$group\n";
-print "Using Access Zone: \t$zone\nHDFS_ROOT set to \t$hdfs_root\n";
+print "Version: $version\n";
+print "passwd file $passwd\n";
+print "group file $group\n";
+print "Using Access Zone: $zone with HDFS ROOT set to $hdfs_root\n";
+print "The distribution is: $dist\n\n";
+if ($dist =~ /cdh/) { 
+    $got_super = "FALSE";
+}
 #
 #   First do the groups they need to exist before the users
 #
-open (CMD,$group) || die "Problem with the group file. Error $!\n";
+open (CMD,$group) || die "ERROR opening $group <$!>";
 while (<CMD>) {
     chomp;
     ($grpname,$p,$gid,$users)= split /\:/,$_;
     print "Group: $grpname,\t\tGID: $gid,\tusers: $users\n";
     if ($grpname) {
-	push @commands ,"isi auth groups create $grpname --gid $gid  --provider local --zone $zone\n" ;
+      push @commands ,"isi auth groups create $grpname --gid $gid  --provider local --zone $zone\n" ;
     }
-#
-#  so if there are multiple users in a group pick them up here.
-#
+    #
+    #  so if there are multiple users in a group pick them up here.
+    #
     if ($users) {
 	my @super = split /\,/,$users;
 	foreach my $u (@super){
-	    push @commands,"isi auth groups modify $grpname --add-user $u --provider local --zone $zone \n";
+	    push @mod_commands,"isi auth groups modify $grpname --add-user $u --provider local --zone $zone \n";
 	}
     }
 }
@@ -100,7 +106,7 @@ while (<CMD>) {
     ($name,$p,$uid,$def_gid)= split /\:/,$_;
     if ($name){
 	print "User: $name,\t\tUID: $uid,\tGID: $def_gid\n";
-	push @commands, "isi auth users create $name --uid $uid --primary-group-gid $def_gid --zone $zone --provider local --home-directory $hdfs_root/user/$name\n";
+	push @commands, "isi auth users create $name --uid $uid --primary-group-gid $def_gid --zone $zone --enabled true  --provider local --home-directory $hdfs_root/user/$name\n";
     }
 }
 close CMD;
@@ -121,13 +127,81 @@ foreach (@file) {
     print OUT $_   ;
 }
 #
-@file = grep (/users create/, @commands);
+@file = grep (/users/, @commands);
 foreach (@file) { 
     print OUT $_   ;
 }
 #
-@file = grep (/groups modify/, @commands);
+@file = grep (/groups modify/, @mod_commands);
 foreach (@file) { 
     print OUT $_   ;
+}
+#
+#  Now write out the commands to create directories
+#
+if ($dist =~ /hdp/) { 
+    print OUT <<EOF;
+#
+# now do the directories...
+# start in hdfs_root, $hdfs_root
+# also in order to use the identities use isi_run -z <zone id> prior to these next commands.
+#
+cd $hdfs_root
+chmod 755 $hdfs_root
+chown hdfs:hadoop $hdfs_root
+mkdir -p -m 1777 app-logs
+chown yarn:hadoop app-logs
+mkdir -p -m 770  app-logs/ambari-qa
+chown ambari-qa:hadoop  app-logs/ambari-qa
+mkdir -p -m 770 app-logs/ambari-qa/logs
+chown ambari-qa:hadoop  app-logs/ambari-qa/logs
+mkdir -m 755 apps  
+chown hdfs:hadoop apps
+mkdir -p -m 750 apps/accumulo
+chown accumulo:hadoop apps/accumulo
+mkdir -p -m 777 apps/falcon
+chown falcon:hdfs apps/falcon
+mkdir -p -m 755 apps/hbase
+chown hdfs:hadoop apps/hbase
+mkdir -p -m 775 apps/hbase/data
+chown hbase:hadoop apps/hbase/data
+mkdir -p -m 711 apps/hbase/staging
+chown hbase:hadoop apps/hbase/staging
+mkdir -p -m 755 apps/hive
+chown hdfs:hdfs apps/hive
+mkdir -p -m 777 apps/hive/warehouse
+chown hive:hdfs apps/hive/warehouse
+mkdir -p -m 755 apps/tez
+chown tez:hdfs apps/tez
+mkdir -p -m 755 apps/webhcat
+chown hcat:hdfs apps/webhcat
+mkdir -p -m 755 ats
+chown yarn:hadoop ats
+mkdir -p -m 775 ats/done
+chown yarn:hadoop ats/done
+mkdir -p -m 755 atsv2
+chown yarn-ats:hadoop atsv2
+mkdir -p -m 755 mapred
+chown mapred:hadoop mapred
+mkdir -p -m 755 mapred/system
+chown mapred:hadoop mapred/system
+mkdir -p -m 755 system
+mkdir -p -m 755 system/yarn
+mkdir -p -m 700 system/yarn/node-labels
+chown -R yarn:hadoop system
+mkdir -m 1777 tmp
+chown hdfs:hdfs tmp
+mkdir -p -m 777 tmp/hive
+chown ambari-qa:hadoop tmp/hive
+chmod 755 user
+chown hdfs:hdfs user
+chmod 755 user/ambari-qa
+chmod 755 user/hcat
+chmod 755 user/hdfs
+chmod 700 user/hive
+chmod 755 user/hue
+chmod 775 user/oozie
+chmod 755 user/yarn
+EOF
 }
 close OUT;
